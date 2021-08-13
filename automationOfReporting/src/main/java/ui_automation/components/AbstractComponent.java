fbda7e5691a65361ab0f3b7a163e61e5d2e7b119
Tarun <saitaruna@testvagrant.com>
2021-08-13 23:15:13 +0530

    created abstract common class for components

---
+++ automationOfReporting/src/main/java/ui_automation/components/AbstractComponent.java
@@ -0,0 +1,14 @@
+package ui_automation.components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class AbstractComponent {
+
+    protected WebDriver driver;
+
+    public AbstractComponent(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
