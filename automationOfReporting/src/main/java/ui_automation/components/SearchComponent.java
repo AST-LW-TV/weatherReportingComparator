43eee3745d79ed29e3f9bae8f651b06bab557b2b
Tarun <saitaruna@testvagrant.com>
2021-08-13 23:21:05 +0530

    search field component common in both home and result page

---
+++ automationOfReporting/src/main/java/ui_automation/components/SearchComponent.java
@@ -0,0 +1,42 @@
+package ui_automation.components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utilities.ExplicitDriverWaits;
+
+import java.util.List;
+
+public class SearchComponent extends AbstractComponent {
+
+    @FindBy(css="input[name='query']")
+    WebElement searchField;
+
+    @FindBy(css=".search-result")
+    List<WebElement> obtainedPlaceResults;
+
+    public SearchComponent(WebDriver driver){
+        super(driver);
+    }
+
+    public void enterPlaceNameByCharacter(String place){
+        int size=place.length();
+        for(int i=0;i<size-(size-3);i++){  // only three char are entered, so that we get suggestions with partial text
+            searchField.sendKeys(place.charAt(i)+"");  // converting to string
+        }
+    }
+
+    public int getNumberOfPlaceSuggestions(String place){
+        searchField.clear();
+        enterPlaceNameByCharacter(place);
+        ExplicitDriverWaits.waitForDropDownVisibility(driver,obtainedPlaceResults);
+        return obtainedPlaceResults.size();
+    }
+
+    public void selectFirstOptionFromDropDown(String place){
+        searchField.clear();
+        enterPlaceNameByCharacter(place);
+        obtainedPlaceResults.get(0).click();
+    }
+
+}
