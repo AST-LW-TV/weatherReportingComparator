558b9938deadc68999e04f50d8c9be59fbe2ad91
Tarun <saitaruna@testvagrant.com>
2021-08-13 23:13:32 +0530

    added another wait for displaying the dropdown

--- automationOfReporting/src/main/java/utilities/ExplicitDriverWaits.java
+++ automationOfReporting/src/main/java/utilities/ExplicitDriverWaits.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class ExplicitDriverWaits {
 
     private static WebDriverWait wait;
     private static WebDriver driverToWait;
-    private static WebElement webElement;
 
     private static void setUp(WebDriver driver) {
         driverToWait = driver;
@@ -17,8 +18,12 @@ public class ExplicitDriverWaits {
     }
 
     public static WebElement visibility(WebDriver driver, WebElement element) {
-        webElement = null;
         setUp(driver);
         return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void waitForDropDownVisibility(WebDriver driver, List<WebElement> element) {
+        setUp(driver);
+        wait.until((driverToWait) -> (element.size() >= 3 && element.size() <= 5));
     }
 }
