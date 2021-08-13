76ee7643651f54c0f1ae9590a655d1707c14c68c
Tarun <saitaruna@testvagrant.com>
2021-08-13 13:55:34 +0530

    minor bug and refactoring

--- automationOfReporting/src/main/java/utilities/ReadFilePaths.java
+++ automationOfReporting/src/main/java/utilities/ReadFilePaths.java
@@ -7,9 +7,9 @@ import java.util.function.Supplier;
 public class ReadFilePaths {
 
     // file path constants used in main
-    private static final Supplier<String> CP = () -> "./src/main/resources/ui_properties/chrome.properties";
-    private static final Supplier<String> FP = () -> "./src/main/resources/ui_properties/firefox.properties";
-    private static final Supplier<String> BP = () -> "./src/main/resources/ui_properties/common/browserProperties.properties";
+    private static final Supplier<String> CP = () -> "./src/main/resources/ui_properties/browser/chrome.properties";
+    private static final Supplier<String> FP = () -> "./src/main/resources/ui_properties/browser/firefox.properties";
+    private static final Supplier<String> BP = () -> "./src/main/resources/ui_properties/browser/common/browserProperties.properties";
 
     // file path constants used in test
     private static final Supplier<String> SS = () -> "./src/test/screenshots";
