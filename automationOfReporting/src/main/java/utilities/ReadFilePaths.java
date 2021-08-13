082bf6b4173254eea605abac517feab3cdee0e42
Tarun <saitaruna@testvagrant.com>
2021-08-13 13:40:28 +0530

    added new path constant

--- automationOfReporting/src/main/java/utilities/ReadFilePaths.java
+++ automationOfReporting/src/main/java/utilities/ReadFilePaths.java
@@ -6,18 +6,24 @@ import java.util.function.Supplier;
 
 public class ReadFilePaths {
 
-    // file path constants
-    private static final Supplier<String> BPF=
-            () -> "./src/main/resources/browserProperties.properties";
+    // file path constants used in main
+    private static final Supplier<String> CP = () -> "./src/main/resources/ui_properties/chrome.properties";
+    private static final Supplier<String> FP = () -> "./src/main/resources/ui_properties/firefox.properties";
+    private static final Supplier<String> BP = () -> "./src/main/resources/ui_properties/common/browserProperties.properties";
 
-    private static final Map<String,Supplier<String>> MAP=new HashMap<>();
+    // file path constants used in test
+    private static final Supplier<String> SS = () -> "./src/test/screenshots";
 
-    static{
-        MAP.put("browserPropertiesPath",BPF);
+    private static final Map<String, Supplier<String>> MAP = new HashMap<>();
+
+    static {
+        MAP.put("chromeProperties", CP);
+        MAP.put("firefoxProperties", FP);
+        MAP.put("browserProperties", BP);
+        MAP.put("screenshotsDir", SS);
     }
 
-    public static String getFilePath(String nameOfTheFile){
+    public static String getFilePath(String nameOfTheFile) {
         return MAP.get(nameOfTheFile).get();
     }
-
 }
